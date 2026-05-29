554d11f785ef7af1e03071bc15bd9b2f0676678a
encloinc <jelch2002@gmail.com>
2026-05-29 22:57:58 +0000

    chore: add fire to prio

--- src/modules/pizzafun/consts.rs
+++ src/modules/pizzafun/consts.rs
@@ -1,4 +1,9 @@
 // Alkane IDs that should be treated as earliest for series naming.
 // Order here determines priority within a series.
-pub const PRIORITY_SERIES_ALKANES: &[(&str, &str)] =
-    &[("2:68479", "tortilla"), ("2:0", "diesel"), ("32:0", "frbtc"), ("2:16", "methane")];
+pub const PRIORITY_SERIES_ALKANES: &[(&str, &str)] = &[
+    ("2:68479", "tortilla"),
+    ("2:0", "diesel"),
+    ("32:0", "frbtc"),
+    ("2:16", "methane"),
+    ("2:77623", "fire"),
+];
